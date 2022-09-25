d6aa828bcc7da1929ff7a33ed2425f9420207e3d
Jayaram_Venkat <jayaramvenkatanarayanan@live.com>
2022-09-25 16:05:29 +0530

    feat: header bar added (#2)

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,8 +1,17 @@
 import React from "react";
-import Navbar from "./Navbar";
+import NavItems from "./NavItems";
 
 function Header() {
-  return <Navbar />;
+  return (
+    <>
+      <header>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>MyShipApp</title>
+      </header>
+      <NavItems />
+    </>
+  );
 }
 
 export default Header;
