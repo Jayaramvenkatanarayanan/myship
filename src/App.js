5b55ff43c6277098864f2b2474cede140bf6cc44
Jayaram <jayaramvenkatanarayanan@live.com>
2022-09-25 19:19:31 +0530

    feat: about us added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
 import NotFoundPage from "./components/NotFoundPage";
 
 export default function App() {
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "./Hero";
+import HomeAboutUs from "./HomeAboutUs";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <HomeAboutUs />
     </div>
   );
 }
